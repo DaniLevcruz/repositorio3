b7cda15f778c7bfface75dd42be6d54c5d1b9c0e
Daniel Levoreiro <danilev511@gmail.com>
2024-01-01 09:57:02 -0600

    promedio 1.5

--- mayoresqueelpromedio/mayorpromedio.js
+++ mayoresqueelpromedio/mayorpromedio.js
@@ -1,18 +1,15 @@
-// Solicitar al usuario la cantidad de números a ingresar
-const cantidadNumeros = parseInt(prompt("Ingrese la cantidad de números:"));
+const cantidadNumeros = parseInt(prompt("¿Cuántos datos ingresará?"));
 
-// Verificar que se ingrese al menos un número
 if (cantidadNumeros <= 0 || isNaN(cantidadNumeros)) {
     console.log("Por favor, ingrese una cantidad válida de números.");
 } else {
-    // Capturar los números ingresados por el usuario y almacenarlos en un array
+    // Capturar los números ingresados
     const numeros = [];
     for (let i = 0; i < cantidadNumeros; i++) {
         const numero = parseFloat(prompt(`Ingrese el número ${i + 1}:`));
         numeros.push(numero);
     }
 
-    // Calcular el promedio de los números ingresados
     const suma = numeros.reduce((total, numero) => total + numero, 0);
     const promedio = suma / cantidadNumeros;
 
